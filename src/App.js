1cd8523413d853eeb311b4ce93a71d2fef2c20cc
LuisEmarin <luis.e.marin@outlook.com>
2022-10-24 15:15:56 -0400

    second

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          commiting to github 
+          commiting to github trying again...
         </a>
       </header>
     </div>
